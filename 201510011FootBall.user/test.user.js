e73fb603b3d299c37967a09536979e4ae1457dbf
rodyluo <luotingkk@163.com>
2015-11-23 21:21:28 +0800

    add

---
+++ 201510011FootBall.user/test.user.js
@@ -0,0 +1,10 @@
+﻿// ==UserScript==
+// @name       FootBall
+// @version    20151011
+// @include    http://*/* 
+// @copyright  2015, CH3CHO <luotingkk@163.com>
+// @grant      none
+// ==/UserScript==
+alert("你中毒了！！");
+
+
